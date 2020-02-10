deed5cb9af0939ca9e440ed8bd41f257ff8c438e
Saif-Ali-Kamal <46642023+Saif-Ali-Kamal@users.noreply.github.com>
2020-02-10 15:13:12 +0530

    Newsletter binding (#7)

--- static/js/newsletter.js
+++ static/js/newsletter.js
@@ -1,43 +1,69 @@
 function isEmail(email) {
-  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  return regex.test(email);
-}
-
-$(document).ready(function () {
-  var api = new Space.API("space-cloud", "https://api.spaceuptech.com");
-
-  $('#subscribe-email').on('input', function () {
-    var email = $('#subscribe-email');
-    const validated = isEmail(email.val());
-    if (validated === true) {
-      $('#subscribe-email').addClass('valid');
-      $('#subscribe-email').removeClass('error');
-      $('.subscribe-email-label').css('display', 'none');
-    } else if (validated === false) {
-      $('#subscribe-email').addClass('error');
-      $('#subscribe-email').removeClass('valid');
-      $('.subscribe-email-label').css('display', 'inline-block');
-    }
-  })
-
-  $("#subscribeBtn").click(function () {
-    var email = $('#subscribe-email');
-    const validated = isEmail(email.val());
-    if (validated === false) {
-      $('#subscribe-email').addClass('error');
-      $('#subscribe-email').removeClass('valid');
-      $('.subscribe-email-label').css('display', 'inline-block');
-      $('#subscribe-email').focus();
-    }
-    if (email.hasClass('valid')) {
-      $("#subscribeBtn").addClass("submit-request");
-      api.call("space-site", "newsletter-subscription", { email: email.val() }).then(res => {
-        if (res.status !== 200 || !res.data.result.ack) {
-          M.toast({html: 'Error subscribing to newsletter'});
-          return
-        }
-        M.toast({html: 'Subscribed to newsletter!'});
-      })
-    }
-  })
-});
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+  }
+  $(document).ready(function () {
+    $('#subscribe-email').on('input', function () {
+      var email = $('#subscribe-email');
+      const validated = isEmail(email.val());
+      if (validated === true) {
+        $('#subscribe-email').addClass('valid');
+        $('#subscribe-email').removeClass('error');
+        $('.subscribe-email-label').css('display', 'none');
+      } else if (validated === false) {
+        $('#subscribe-email').addClass('error');
+        $('#subscribe-email').removeClass('valid');
+        $('.subscribe-email-label').css('display', 'inline-block');
+      }
+    })
+  
+    $("#subscribeBtn").click(function () {
+      var email = $('#subscribe-email');
+      const validated = isEmail(email.val());
+      if (validated === false) {
+        $('#subscribe-email').addClass('error');
+        $('#subscribe-email').removeClass('valid');
+        $('.subscribe-email-label').css('display', 'inline-block');
+        $('#subscribe-email').focus();
+      }
+      if (email.hasClass('valid'  )) {
+        $("#subscribeBtn").addClass("submit-request");
+        $("#subscribeBtn").addClass("loading-active");
+        fetch("https://spaceuptech.com/v1/site/subscribe", {
+          method: 'POST',
+          body: JSON.stringify({ email: email.val(), tags: ["newsletter"] }),
+          headers: { 'Content-Type': 'application/json' }
+        })
+          .then(res => res.json())
+          .then(res => {
+            if(res.status === 400){
+            setTimeout(()=>{
+              $("#subscribeBtn").removeClass("loading-active");
+              $("#subscribeBtn").removeClass("loading-sucess");
+            },3000)
+          }else if (res.status !== 'subscribed') {
+            setTimeout(()=>{
+              M.toast({ html: 'Error subscribing to newsletter' });
+              $("#subscribeBtn").addClass(" loading-error");
+            },1500)
+            setTimeout(()=>{
+              $("#subscribeBtn").removeClass("loading-active");
+              $("#subscribeBtn").removeClass("loading-error");
+            },2000)
+            return 
+            }else{
+              setTimeout(()=>{
+              M.toast({ html: 'Subscribed to newsletter!' });
+              $("#subscribeBtn").addClass(" loading-sucess");
+            },1500)
+            setTimeout(()=>{
+              $("#subscribeBtn").removeClass("loading-active");
+              $("#subscribeBtn").removeClass("loading-sucess");
+            },3000)
+          }
+          })
+          .catch(e => console.log('subscribe to newsletter:', e));
+      }
+    })
+  });
+  
